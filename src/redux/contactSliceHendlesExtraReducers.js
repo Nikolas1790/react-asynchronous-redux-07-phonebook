08368524fe8c14753c58409476fe16d8e26190e5
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-10-08 21:08:54 +0300

    s

--- src/redux/contactSliceHendlesExtraReducers.js
+++ src/redux/contactSliceHendlesExtraReducers.js
@@ -1,11 +1,13 @@
 export const handlePending = state => {
     state.isLoading = true;
-  };
-  export const handleRejected = (state, action) => {
+};
+
+export const handleRejected = (state, action) => {
     state.isLoading = false;
     state.error = action.payload;
-  };
-  export const handleFulfilledErrorLoading = state => {
+};
+
+export const handleFulfilledErrorLoading = state => {
     state.isLoading = false;
     state.error = null;
-  }
+}
